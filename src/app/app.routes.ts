bb13b7aa33e7e87623f5612bb50e0bc99edec882
Nilesh <nshinde@inferyx.com>
2024-05-11 13:30:37 +0530

    Added Basic rounting for login and chat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+ChatComponent
+LoginComponent
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'chat', component: ChatComponent },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
