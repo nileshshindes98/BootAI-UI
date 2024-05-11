f5cb17eeea43f7556a3f9baa011a1c5497a9601d
Nilesh <nshinde@inferyx.com>
2024-05-11 16:19:37 +0530

    Added chat template

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule,MatInputModule,FormsModule,MatIconModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{ 
+  message: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  sendMessage(): void {
+ 
+    console.log('Sending message:', this.message);
+
+    this.sendGeminiMessage(this.message);
+
+    this.message = '';
+  }
+
+  private sendGeminiMessage(message: string) {
+
+  }
 }
